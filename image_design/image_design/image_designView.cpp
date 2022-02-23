579f46d93340ac55b7f87626e5bf60ef41f1a741
益民 杨 <18810580621@163.com>
2022-02-23 12:08:38 +0800

    default code

---
+++ image_design/image_design/image_designView.cpp
@@ -0,0 +1,128 @@
+﻿
+// image_designView.cpp: CimagedesignView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "image_design.h"
+#endif
+
+#include "image_designDoc.h"
+#include "image_designView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CimagedesignView
+
+IMPLEMENT_DYNCREATE(CimagedesignView, CView)
+
+BEGIN_MESSAGE_MAP(CimagedesignView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CimagedesignView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CimagedesignView 构造/析构
+
+CimagedesignView::CimagedesignView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CimagedesignView::~CimagedesignView()
+{
+}
+
+BOOL CimagedesignView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CimagedesignView 绘图
+
+void CimagedesignView::OnDraw(CDC* /*pDC*/)
+{
+	CimagedesignDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CimagedesignView 打印
+
+
+void CimagedesignView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CimagedesignView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CimagedesignView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CimagedesignView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CimagedesignView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CimagedesignView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CimagedesignView 诊断
+
+#ifdef _DEBUG
+void CimagedesignView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CimagedesignView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CimagedesignDoc* CimagedesignView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CimagedesignDoc)));
+	return (CimagedesignDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CimagedesignView 消息处理程序
